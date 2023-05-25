70fc1e5edf85bdf26f86578c37ddbfba6ce1978f
Artyom V <91020416+OutMeister@users.noreply.github.com>
2023-05-25 22:57:15 +0300

    Update task_12_2.py

--- tasks_9_11_12/task12/task_12_2.py
+++ tasks_9_11_12/task12/task_12_2.py
@@ -34,3 +34,23 @@
  '172.21.41.129', '172.21.41.130', '172.21.41.131', '172.21.41.132']
 
 """
+import ipaddress
+
+
+def convert_ranges_to_ip_list(ip_addresses):
+    ip_list = []
+    for ip_address in ip_addresses:
+        if "-" in ip_address:
+            start_ip, end_ip = ip_address.split("-")
+            if "." not in end_ip:
+                end_ip = ".".join(start_ip.split(".")[:-1] + [end_ip])
+            start_ip = ipaddress.ip_address(start_ip)
+            end_ip = ipaddress.ip_address(end_ip)
+            for ip in range(int(start_ip), int(end_ip) + 1):
+                ip_list.append(str(ipaddress.ip_address(ip)))
+        else:
+            ip_list.append(ip_address)
+    return ip_list
+
+if __name__ == "__main__":
+    print(convert_ranges_to_ip_list(['8.8.4.4', '1.1.1.1-3', '172.21.41.128-172.21.41.132']))
