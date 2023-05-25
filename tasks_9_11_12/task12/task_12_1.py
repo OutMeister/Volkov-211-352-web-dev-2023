c742b17203353d14ff46f4ae1a39e694bb732936
Artyom V <91020416+OutMeister@users.noreply.github.com>
2023-05-25 22:57:00 +0300

    Update task_12_1.py

--- tasks_9_11_12/task12/task_12_1.py
+++ tasks_9_11_12/task12/task_12_1.py
@@ -17,3 +17,22 @@ IP-адрес считается доступным, если выполнени
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+import subprocess
+
+def ping_ip_addresses(ip_addresses):
+    reachable = []
+    unreachable = []
+
+    for ip in ip_addresses:
+        result = subprocess.run(["ping", "-c", "3", ip])
+        if result.returncode == 0:
+            reachable.append(ip)
+        else:
+            unreachable.append(ip)
+
+    res = (reachable, unreachable)
+    return res
+
+
+if __name__ == "__main__":
+    print(ping_ip_addresses(["10.1.1.1", "8.8.8.8", "10.0.0.0"]))
