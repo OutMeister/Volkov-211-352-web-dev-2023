916447d232b4ce348e3c5a724d1fdaec941dfeef
Artyom V <91020416+OutMeister@users.noreply.github.com>
2023-05-25 22:52:30 +0300

    Update task_9_1a.py

--- tasks_9_11_12/task9/task_9_1a.py
+++ tasks_9_11_12/task9/task_9_1a.py
@@ -43,3 +43,27 @@ port_security_template = [
 ]
 
 access_config = {"FastEthernet0/12": 10, "FastEthernet0/14": 11, "FastEthernet0/16": 17}
+
+def generate_access_config(intf_vlan_mapping, access_template, psecurity=None):
+    """
+    intf_vlan_mapping - словарь с соответствием интерфейс-VLAN такого вида:
+        {'FastEthernet0/12':10,
+         'FastEthernet0/14':11,
+         'FastEthernet0/16':17}
+    access_template - список команд для порта в режиме access
+
+    Возвращает список всех портов в режиме access с конфигурацией на основе шаблона
+    """
+    list_result=[]
+    for k, v in intf_vlan_mapping.items():
+        list_result.append('interface '+k)
+        for command in access_template:
+            if 'access vlan' in command:
+                list_rszult.append(command + ' ' + str(v))
+            else:
+                list_result.append(command)
+        if psecurity:
+            list_result+=psecurity
+    return list_result
+print(generate_access_config(access_config, access_mode_template))
+print(generate_access_config(access_config, access_mode_template, port_security_template))
