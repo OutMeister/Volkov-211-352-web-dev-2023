52886488270053205a3637c2978609d6bab51e42
Artyom V <91020416+OutMeister@users.noreply.github.com>
2023-10-09 10:52:45 +0300

    Update task_4_8.py

--- tasks_4567/task4/task_4_8.py
+++ tasks_4567/task4/task_4_8.py
@@ -25,10 +25,8 @@
 проверять результат.
 """
 ip = "192.168.3.1"
-octets = ip.split(".")
 
-output = """
-{0:<10}{1:<10}{2:<10}{3:<10}
-{0:08b}  {1:08b}  {2:08b}  {3:08b}"""
-print(output.format(int(octets[0]), int(octets[1]), int(octets[2]), int(octets[3])))
+ip_array = ip.split(".")
 
+print("{:<10}{:<10}{:<10}{:<10}".format(int(ip_array[0]), int(ip_array[1]), int(ip_array[2]), int(ip_array[3])))
+print("{:08b}  {:08b}  {:08b}  {:08b}".format(int(ip_array[0]), int(ip_array[1]), int(ip_array[2]), int(ip_array[3])))
