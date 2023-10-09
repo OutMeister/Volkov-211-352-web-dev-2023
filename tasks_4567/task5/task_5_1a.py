eb2c76d95079f32a9f876962348f2e9840de977a
Artyom V <91020416+OutMeister@users.noreply.github.com>
2023-10-09 10:55:34 +0300

    Update task_5_1a.py

--- tasks_4567/task5/task_5_1a.py
+++ tasks_4567/task5/task_5_1a.py
@@ -25,14 +25,14 @@ london_co = {
         "vendor": "Cisco",
         "model": "4451",
         "ios": "15.4",
-        "ip": "10.255.0.1",
+        "ip": "10.255.0.1"
     },
     "r2": {
         "location": "21 New Globe Walk",
         "vendor": "Cisco",
         "model": "4451",
         "ios": "15.4",
-        "ip": "10.255.0.2",
+        "ip": "10.255.0.2"
     },
     "sw1": {
         "location": "21 New Globe Walk",
@@ -41,8 +41,11 @@ london_co = {
         "ios": "3.6.XE",
         "ip": "10.255.0.101",
         "vlans": "10,20,30",
-        "routing": True,
-    },
+        "routing": True
+    }
 }
-
-print(london_co[name][param_of_name])
+print('\n' + '-' * 30)
+name = input("Введите имя устройства: ")
+param = input("Введите имя параметра: ")
+print('\n' + '-' * 30)
+print(london_co[name][param])
